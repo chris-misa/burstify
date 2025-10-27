cca1ffbea05cb0be7e349842b6fa33bb2d8dee00
Chris Misa <cmisa@cs.uoregon.edu>
2025-10-27 14:54:52 -0700

    added initial address structure stuff

---
+++ src/addrs.zig
@@ -0,0 +1,128 @@
+//! Copyright: 2025 Chris Misa
+//! License: (See ./LICENSE)
+//!
+//! Functions for dealing with IPv4 addresses
+//!
+
+const std = @import("std");
+
+
+pub const Prefix = struct { base: u32, len: u32 };
+
+pub const PrefixMap = struct {
+    data: []std.AutoHashMap(u32, f64),
+    n: u32,
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) error{OutOfMemory}!PrefixMap {
+        const data = try allocator.alloc(std.AutoHashMap(u32, f64), 33);
+        for (data) |*elem| {
+            elem.* = std.AutoHashMap(u32, f64).init(allocator);
+        }
+        return PrefixMap{ .data = data, .n = 0, .allocator = allocator };
+    }
+
+    pub fn deinit(self: *PrefixMap) void {
+        for (self.data) |*elem| {
+            elem.deinit();
+        }
+        self.allocator.free(self.data);
+    }
+
+    ///
+    /// Adds the given address to the prefix map
+    /// (After all addresses are added, prefixify() must be called to actually form the map)
+    ///
+    pub fn addAddr(self: *PrefixMap, addr: u32) error{OutOfMemory}!void {
+        try self.data[32].put(addr, 1.0);
+        self.*.n += 1;
+    }
+
+    ///
+    /// Form the prefix map for the addresses already added.
+    /// Returns the total number of addresses.
+    ///
+    fn prefixify(self: *PrefixMap) error{OutOfMemory}!void {
+        const m = self.*.data;
+        // Sum children
+        for (0..32) |i| {
+            const pl = 32 - i;
+            const mask: u32 = @truncate(@as(u64, 0xFFFFFFFF) << @truncate(i + 1));
+            var it = m[pl].iterator();
+            while (it.next()) |elem| {
+                const addr = elem.key_ptr.* & mask;
+                if (m[pl - 1].getPtr(addr)) |next_elem| {
+                    next_elem.* += elem.value_ptr.*;
+                } else {
+                    try m[pl - 1].put(addr, elem.value_ptr.*);
+                }
+            }
+        }
+
+        // Normalize
+        const n = @as(f64, @floatFromInt(self.n));
+        for (0..33) |pl| {
+            var it = m[pl].valueIterator();
+            while (it.next()) |v| {
+                v.* /= n;
+            }
+        }
+    }
+
+    ///
+    /// Look up the weight (w) of how much mass goes left at the given prefix.
+    /// (Note that how much mass goes right is just (1.0 - w).)
+    ///
+    fn get_w(self: *PrefixMap, pfx: Prefix) f64 {
+        const m = self.*.data;
+        const pl = pfx.len + 1;
+        const l_base: u32 = pfx.base;
+        const r_base: u32 = pfx.base | (@as(u32, 1) << @truncate(32 - pl));
+
+        const l: f64 = if (m[pl].get(l_base)) |v| v else 0.0;
+        const r: f64 = if (m[pl].get(r_base)) |v| v else 0.0;
+
+        return l / (l + r);
+    }
+
+    ///
+    /// Performs a symmetric logit-normal fit of the weights of the given prefix map.
+    /// Returns the fit value of sigma.
+    ///
+    pub fn logit_normal_fit(self: *PrefixMap) error{OutOfMemory}!f64 {
+        const m = self.*.data;
+        const n: f64 = @as(f64, @floatFromInt(self.n));
+        
+        try self.prefixify();
+
+        var count: u32 = 0;
+        var m1: f64 = 0.0;
+        var m2: f64 = 0.0;
+        
+        for (0..32) |pl| {
+            var it = m[pl].iterator();
+            while (it.next()) |elem| {
+                // Skip the singletons
+                if (elem.value_ptr.* > 1.0 / n) {
+                    var w = self.get_w(.{ .base = elem.key_ptr.*, .len = @intCast(pl) });
+                    if (w == 0.0) {
+                        w = 1.0 / (2.0 * n);
+                    }
+                    if (w == 1.0) {
+                        w = 1.0 - (1.0 / (2.0 * n));
+                    }
+                    const x = @log(w / (1.0 - w));
+
+                    // Welford's algorithm...
+                    count += 1;
+                    const d = x - m1;
+                    m1 += d / @as(f64, @floatFromInt(count));
+                    const d2 = x - m1;
+                    m2 += d * d2;
+                }
+            }
+        }
+        return @sqrt(m2 / @as(f64, @floatFromInt(count - 1)));
+    }
+};
+
--- src/main.zig
+++ src/main.zig
@@ -7,87 +7,9 @@
 const std = @import("std");
 
 const pcap = @cImport(@cInclude("pcap/pcap.h"));
+
 const h = @import("parse_headers.zig");
-
-const Key = struct { saddr: u32, daddr: u32 };
-
-const Val = struct { sport: u16, dport: u16, proto: u8, len: u16, tcpflags: u8 };
-
-const Burst = struct { first: f64, last: f64, counts: std.AutoHashMap(Val, u32) };
-
-const State = std.AutoHashMap(Key, std.ArrayList(Burst));
-
-const burst_timeout_sec = 0.01;
-
-fn onePacket(dlt: i32, pcap_hdr: pcap.pcap_pkthdr, pkt: [*c]const u8, state: *State, allocator: std.mem.Allocator) error{OutOfMemory}!void {
-    const t: f64 =
-        @as(f64, @floatFromInt(pcap_hdr.ts.tv_sec)) +
-        @as(f64, @floatFromInt(pcap_hdr.ts.tv_usec)) / 1000000.0;
-
-    var p: h.struct_headers = .{};
-
-    _ = h.parse_headers(dlt == pcap.DLT_EN10MB, pkt, pkt + pcap_hdr.caplen, &p);
-
-    // Only look at ipv4 packets (for now)
-    if (p.ipv4) |ipv4| {
-
-        // Project the fields we need
-        const key: Key = .{ .saddr = ipv4.saddr, .daddr = ipv4.daddr };
-
-        const sport = if (p.tcp) |tcp| tcp.source else if (p.udp) |udp| udp.source else 0;
-        const dport = if (p.tcp) |tcp| tcp.dest else if (p.udp) |udp| udp.dest else 0;
-        const tcpflags = if (p.tcp) |tcp| tcp.flags else 0;
-        const val: Val = .{ .sport = @byteSwap(sport), .dport = @byteSwap(dport), .proto = ipv4.protocol, .len = @byteSwap(ipv4.tot_len), .tcpflags = tcpflags };
-
-        if (state.getPtr(key)) |*bursts| {
-            // Previously-observed key
-            var burst: *Burst = &bursts.*.items[bursts.*.items.len - 1];
-            if (t - burst.last >= burst_timeout_sec) {
-                // Handle burst timeout
-                var counts = std.AutoHashMap(Val, u32).init(allocator);
-                try counts.put(val, 1);
-                const new_burst: Burst = .{ .first = t, .last = t, .counts = counts };
-                try bursts.*.append(new_burst);
-            } else {
-                // Add packet to burst
-                if (burst.counts.getPtr(val)) |count| {
-                    count.* += 1;
-                } else {
-                    try burst.counts.put(val, 1);
-                }
-                burst.last = t;
-            }
-        } else {
-            // First-time observing this key
-            var counts = std.AutoHashMap(Val, u32).init(allocator);
-            try counts.put(val, 1);
-            const new_burst: Burst = .{ .first = t, .last = t, .counts = counts };
-
-            var bursts = std.ArrayList(Burst).init(allocator);
-            try bursts.append(new_burst);
-            try state.put(key, bursts);
-        }
-    }
-}
-
-fn printState(state: *State) void {
-    var it = state.iterator();
-    while (it.next()) |elem| {
-        const key = elem.key_ptr;
-        const bursts = elem.value_ptr;
-
-        const src = std.net.Ip4Address.init(@bitCast(key.saddr), 0);
-        const dst = std.net.Ip4Address.init(@bitCast(key.daddr), 0);
-        std.debug.print("{} -> {}\n", .{ src, dst });
-        for (bursts.items) |burst| {
-            std.debug.print("  {d:.6} {d:.6}\n", .{ burst.first, burst.last - burst.first });
-            var it2 = burst.counts.iterator();
-            while (it2.next()) |elem2| {
-                std.debug.print("    {}: {}\n", .{ elem2.key_ptr, elem2.value_ptr.* });
-            }
-        }
-    }
-}
+const addr = @import("addrs.zig");
 
 pub fn main() !void {
     if (std.os.argv.len != 2) {
@@ -122,26 +44,31 @@ pub fn main() !void {
     var pkt: [*c]const u8 = undefined;
     var pcap_hdr: pcap.pcap_pkthdr = undefined;
 
-    var state = State.init(allocator);
-    defer {
-        var it = state.valueIterator();
-        while (it.next()) |bursts| {
-            for (bursts.items) |*burst| {
-                burst.*.counts.deinit();
-            }
-            bursts.deinit();
-        }
-        state.deinit();
-    }
+    var pfxs = try addr.PrefixMap.init(allocator);
+    defer pfxs.deinit();
 
     while (true) {
         pkt = pcap.pcap_next(hdl, &pcap_hdr);
         if (pkt == null) {
             break;
         }
-        try onePacket(dlt, pcap_hdr, pkt, &state, allocator);
-        // TODO: if we actually run out of memory, we should break the loop and exit...
+        try onePacket(dlt, pcap_hdr, pkt, &pfxs);
     }
+    try finish(&pfxs);
+}
 
-    printState(&state);
+fn onePacket(dlt: i32, pcap_hdr: pcap.pcap_pkthdr, pkt: [*c]const u8, pfxs: *addr.PrefixMap) error{OutOfMemory}!void {
+    var p: h.struct_headers = .{};
+
+    _ = h.parse_headers(dlt == pcap.DLT_EN10MB, pkt, pkt + pcap_hdr.caplen, &p);
+
+    // Only look at ipv4 packets (for now)
+    if (p.ipv4) |ipv4| {
+        try pfxs.addAddr(ipv4.saddr);
+    }
+}
+
+fn finish(pfxs: *addr.PrefixMap) error{OutOfMemory}!void {
+    const sigma = try pfxs.logit_normal_fit();
+    std.debug.print("sigma = {d:.6}\n", .{ sigma });
 }
