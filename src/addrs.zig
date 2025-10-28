4b398e0b5a6b0854daca308af5fac2ea931c3bd4
Chris Misa <cmisa@cs.uoregon.edu>
2025-10-27 23:11:35 -0700

    spilling address generator

--- src/addrs.zig
+++ src/addrs.zig
@@ -8,6 +8,13 @@ const std = @import("std");
 
 pub const Prefix = struct { base: u32, len: u32 };
 
+///
+/// PrefixMaps accumulate addresses (using addAddr()), form a conservative cascade,
+/// and fit the distribution of the weights to a symmetric logit-normal distribution (using logit_normal_fit()).
+///
+/// Might eventually add more of the multifractal analysis implementation here, but this is all that's required
+/// for now.
+///
 pub const PrefixMap = struct {
     data: []std.AutoHashMap(u32, f64),
     n: u32,
@@ -40,53 +47,6 @@ pub const PrefixMap = struct {
     }
 
     ///
-    /// Form the prefix map for the addresses already added.
-    /// Returns the total number of addresses.
-    ///
-    fn prefixify(self: *PrefixMap) error{OutOfMemory}!void {
-        const m = self.*.data;
-        // Sum children
-        for (0..32) |i| {
-            const pl = 32 - i;
-            const mask: u32 = @truncate(@as(u64, 0xFFFFFFFF) << @truncate(i + 1));
-            var it = m[pl].iterator();
-            while (it.next()) |elem| {
-                const addr = elem.key_ptr.* & mask;
-                if (m[pl - 1].getPtr(addr)) |next_elem| {
-                    next_elem.* += elem.value_ptr.*;
-                } else {
-                    try m[pl - 1].put(addr, elem.value_ptr.*);
-                }
-            }
-        }
-
-        // Normalize
-        // const n = @as(f64, @floatFromInt(self.n));
-        // for (0..33) |pl| {
-        //     var it = m[pl].valueIterator();
-        //     while (it.next()) |v| {
-        //         v.* /= n;
-        //     }
-        // }
-    }
-
-    ///
-    /// Look up the weight (w) of how much mass goes left at the given prefix.
-    /// (Note that how much mass goes right is just (1.0 - w).)
-    ///
-    fn get_w(self: *PrefixMap, pfx: Prefix) f64 {
-        const m = self.*.data;
-        const pl = pfx.len + 1;
-        const l_base: u32 = pfx.base;
-        const r_base: u32 = pfx.base | (@as(u32, 1) << @truncate(32 - pl));
-
-        const l: f64 = if (m[pl].get(l_base)) |v| v else 0.0;
-        const r: f64 = if (m[pl].get(r_base)) |v| v else 0.0;
-
-        return l / (l + r);
-    }
-
-    ///
     /// Performs a symmetric logit-normal fit of the weights of the given prefix map.
     /// Returns the fit value of sigma.
     ///
@@ -103,7 +63,6 @@ pub const PrefixMap = struct {
             var it = m[pl].iterator();
             while (it.next()) |elem| {
                 // Skip the singletons
-                // if (elem.value_ptr.* > 1.0 / n) {
                 if (elem.value_ptr.* > 1.0) {
                     var w = self.get_w(.{ .base = elem.key_ptr.*, .len = @intCast(pl) });
                     if (w == 0.0) {
@@ -126,4 +85,110 @@ pub const PrefixMap = struct {
         }
         return @sqrt(m2 / @as(f64, @floatFromInt(count - 1)));
     }
+
+    ///
+    /// Form the prefix map for the addresses already added.
+    /// Returns the total number of addresses.
+    ///
+    fn prefixify(self: *PrefixMap) error{OutOfMemory}!void {
+        const m = self.*.data;
+        // Sum children
+        for (0..32) |i| {
+            const pl = 32 - i;
+            const mask: u32 = @truncate(@as(u64, 0xFFFFFFFF) << @truncate(i + 1));
+            var it = m[pl].iterator();
+            while (it.next()) |elem| {
+                const addr = elem.key_ptr.* & mask;
+                if (m[pl - 1].getPtr(addr)) |next_elem| {
+                    next_elem.* += elem.value_ptr.*;
+                } else {
+                    try m[pl - 1].put(addr, elem.value_ptr.*);
+                }
+            }
+        }
+    }
+
+    ///
+    /// Look up the weight (w) of how much mass goes left at the given prefix.
+    /// (Note that how much mass goes right is just (1.0 - w).)
+    ///
+    fn get_w(self: *PrefixMap, pfx: Prefix) f64 {
+        const m = self.*.data;
+        const pl = pfx.len + 1;
+        const l_base: u32 = pfx.base;
+        const r_base: u32 = pfx.base | (@as(u32, 1) << @truncate(32 - pl));
+
+        const l: f64 = if (m[pl].get(l_base)) |v| v else 0.0;
+        const r: f64 = if (m[pl].get(r_base)) |v| v else 0.0;
+
+        return l / (l + r);
+    }
 };
+
+///
+/// generate() constructs a conservative cascade with symmetric logit-norma(sigma) generator
+/// and samples n addresses.
+///
+/// The caller is responsible for freeing the returned slice.
+///
+pub fn generate(sigma: f64, n: u32, rand: std.Random, allocator: std.mem.Allocator) error{OutOfMemory}!std.ArrayList(u32) {
+    const root = Prefix{ .base = 0, .len = 0 };
+    var res = try std.ArrayList(u32).initCapacity(allocator, n);
+    try gen_rec(sigma, root, n, rand, &res);
+    return res;
+}
+
+fn gen_rec(sigma: f64, pfx: Prefix, n: u32, rand: std.Random, res: *std.ArrayList(u32)) error{OutOfMemory}!void {
+    if (n == 0) {
+        return;
+    } else if (pfx.len == 32) {
+        try res.append(pfx.base);
+    } else {
+        const x: f64 = rand.floatNorm(f64) * sigma;
+        const w: f64 = 1.0 / (1 + @exp(-x));
+        const left_count: u32 = @intFromFloat(@round(@as(f64, @floatFromInt(n)) * w));
+        const right_count: u32 = @intFromFloat(@round(@as(f64, @floatFromInt(n)) * (1.0 - w)));
+        const left = Prefix{ .base = pfx.base, .len = pfx.len + 1 };
+        const right = Prefix{ .base = pfx.base | (@as(u32, 1) << @truncate(32 - (pfx.len + 1))), .len = pfx.len + 1 };
+        const left_count2, const right_count2 = balance(left, left_count, right, right_count);
+        try gen_rec(sigma, left, left_count2, rand, res);
+        try gen_rec(sigma, right, right_count2, rand, res);
+    }
+}
+
+///
+/// Get number of addresses that can fit in this prefix.
+///
+fn getCapacity(pfx: Prefix) u32 {
+    return @truncate(@as(u64, 1) << @truncate(32 - pfx.len));
+}
+
+///
+/// Balance the given left and right prefix counts.
+/// Returns { left_count, right_count} such that both are below their respective capacities.
+///
+fn balance(left: Prefix, left_count: u32, right: Prefix, right_count: u32) struct { u32, u32 } {
+    const left_cap = getCapacity(left);
+    const right_cap = getCapacity(right);
+    var left_final: u32 = undefined;
+    var right_final: u32 = undefined;
+
+    if (@as(u64, left_count) + @as(u64, right_count) > @as(u64, left_cap) + @as(u64, right_cap)) {
+        // A balance is not possible...
+        std.debug.print("ERROR: trying to balance more addresses than total capacity ({} > {})\n", .{ left_count + right_count, left_cap + right_cap });
+        @panic("...this shouldn't happen");
+    } else if (left_count > left_cap) {
+        // Spill from left to right
+        left_final = left_cap;
+        right_final = right_count + (left_count - left_cap);
+    } else if (right_count > right_cap) {
+        // Spill from right to left
+        left_final = left_count + (right_count - right_cap);
+        right_final = right_cap;
+    } else {
+        // No spill
+        left_final = left_count;
+        right_final = right_count;
+    }
+    return .{ left_final, right_final };
+}
